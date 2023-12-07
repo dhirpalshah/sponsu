549194e33e6019cd0cb2b7938aa49d43983c8435
dhirpalshah <86183980+dhirpalshah@users.noreply.github.com>
2023-12-07 23:17:57 +0000

    fixed signup for company employee

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,23 @@
 class ApplicationController < ActionController::Base
   skip_forgery_protection
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   def configure_company_employee_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, :keys => [:email, :company_id])
+    devise_parameter_sanitizer.permit(:sign_up, :keys => [:email, :password, :company_id])
+    devise_parameter_sanitizer.permit(:account_update)
   end
 
   def configure_student_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, :keys => [:email, :gender_id, :university_id])
+    devise_parameter_sanitizer.permit(:sign_up, :keys => [:email, :password, :gender_id, :university_id])
     devise_parameter_sanitizer.permit(:account_update)
+  end
+
+  def configure_permitted_parameters
+    if resource_class == CompanyEmployee 
+      configure_company_employee_permitted_parameters
+    elsif resource_class == Student
+      configure_student_permitted_parameters
+    end
   end
 
   def after_sign_in_path_for(resource)
@@ -20,12 +30,5 @@ class ApplicationController < ActionController::Base
     end
   end
 
-  def configure_permitted_parameters
-    if resource_class == CompanyEmployee 
-      configure_company_employee_permitted_parameters
-    elsif resource_class == Student
-      configure_student_permitted_parameters
-    end
-  end
 
 end
